638a93058918417b0899405fc07298cb40e4f603
João Oliveira <joaopsoliveira2003@gmail.com>
2023-12-09 22:33:31 +0000

    Optimized Code

--- django/website/views.py
+++ django/website/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, mixins
 from django_filters import rest_framework as filters
 from rest_framework.response import Response
 from rest_framework import status
 from .models import *
 from .serializers import *
 
-class ContactViewSet(viewsets.ModelViewSet):
+class CreateOnlyModelViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+  pass
+
+class ContactViewSet(CreateOnlyModelViewSet):
   """
   API endpoint that allows people to contact us.
   """
@@ -14,213 +17,93 @@ class ContactViewSet(viewsets.ModelViewSet):
   serializer_class = ContactSerializer
   permission_classes = []
   filter_backends = (filters.DjangoFilterBackend,)
-  filterset_fields = ['name', 'email', 'subject', 'message']
-  
-  def create(self, request, *args, **kwargs):
-    return super().create(request, *args, **kwargs)
-  
-  def list(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().list(request, *args, **kwargs)
-    return Response({'detail': 'Listing is not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
+  filterset_fields = ContactSerializer.Meta.fields
 
-  def update(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().update(request, *args, **kwargs)
-    return Response({'detail': 'Updates are not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-
-  def destroy(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().destroy(request, *args, **kwargs)
-    return Response({'detail': 'Deletion is not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-
-class FAQViewSet(viewsets.ReadOnlyModelViewSet):
+class FAQViewSet(viewsets.ModelViewSet):
   """
   API endpoint that allows FAQs to be viewed.
   """
   queryset = FAQModel.objects.all()
   serializer_class = FAQSerializer
-  permission_classes = []
+  permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
   filter_backends = (filters.DjangoFilterBackend,)
-  filterset_fields = ['question', 'answer']
-  
-  def create(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().create(request, *args, **kwargs)
-    return Response({'detail': 'Updates are not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-  
-  def list(self, request, *args, **kwargs):
-    return super().list(request, *args, **kwargs)
+  filterset_fields = FAQSerializer.Meta.fields
 
-  def update(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().update(request, *args, **kwargs)
-    return Response({'detail': 'Updates are not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-
-  def destroy(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().destroy(request, *args, **kwargs)
-    return Response({'detail': 'Deletion is not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-  
-class CourseViewSet(viewsets.ModelViewSet):
-  """
-  API endpoint that allows courses to be viewed.
-  """
-  queryset = CourseModel.objects.all()
-  serializer_class = CourseSerializer
-  permission_classes = []
-  filter_backends = (filters.DjangoFilterBackend,)
-  filterset_fields = ['name', 'abbreviation']
-  
-  def create(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().create(request, *args, **kwargs)
-    return Response({'detail': 'Updates are not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-  
-  def list(self, request, *args, **kwargs):
-    return super().list(request, *args, **kwargs)
-
-  def update(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().update(request, *args, **kwargs)
-    return Response({'detail': 'Updates are not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-
-  def destroy(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().destroy(request, *args, **kwargs)
-    return Response({'detail': 'Deletion is not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-  
-class CurricularUnitViewSet(viewsets.ModelViewSet):
-  """
-  API endpoint that allows curricular units to be viewed.
-  """
-  queryset = CurricularUnitModel.objects.all()
-  serializer_class = CurricularUnitSerializer
-  permission_classes = []
-  filter_backends = (filters.DjangoFilterBackend,)
-  filterset_fields = ['name', 'abbreviation', 'course', 'year']
-  
-  def create(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().create(request, *args, **kwargs)
-    return Response({'detail': 'Updates are not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-  
-  def list(self, request, *args, **kwargs):
-    return super().list(request, *args, **kwargs)
-
-  def update(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().update(request, *args, **kwargs)
-    return Response({'detail': 'Updates are not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-
-  def destroy(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().destroy(request, *args, **kwargs)
-    return Response({'detail': 'Deletion is not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-  
-class MaterialTagViewSet(viewsets.ModelViewSet):
-  """
-  API endpoint that allows material tags to be viewed.
-  """
-  queryset = MaterialTagModel.objects.all()
-  serializer_class = MaterialTagSerializer
-  permission_classes = [permissions.IsAuthenticated]
-  filter_backends = (filters.DjangoFilterBackend,)
-  filterset_fields = ['name']
-  
-  def create(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().create(request, *args, **kwargs)
-    return Response({'detail': 'Updates are not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-  
-  def list(self, request, *args, **kwargs):
-    return super().list(request, *args, **kwargs)
-
-  def update(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().update(request, *args, **kwargs)
-    return Response({'detail': 'Updates are not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-
-  def destroy(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().destroy(request, *args, **kwargs)
-    return Response({'detail': 'Deletion is not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-  
-class MaterialViewSet(viewsets.ModelViewSet):
-  """
-  API endpoint that allows materials to be viewed or edited.
-  """
-  queryset = MaterialModel.objects.all()
-  serializer_class = MaterialSerializer
-  permission_classes = [permissions.IsAuthenticated]
-  filter_backends = (filters.DjangoFilterBackend,)
-  filterset_fields = ['name', 'tags']
-  
-  #TODO: Add CRUD methods for this resource.
-  
-class MaterialLinkViewSet(viewsets.ModelViewSet):
-  """
-  API endpoint that allows material links to be viewed or edited.
-  """
-  queryset = MaterialLinkModel.objects.all()
-  serializer_class = MaterialLinkSerializer
-  permission_classes = [permissions.IsAuthenticated]
-  filter_backends = (filters.DjangoFilterBackend,)
-  filterset_fields = ['name']
-  
-  #TODO: Add CRUD methods for this resource.
-  
 class CalendarViewSet(viewsets.ModelViewSet):
   """
   API endpoint that allows calendars to be viewed or edited.
   """
   queryset = CalendarModel.objects.all()
   serializer_class = CalendarSerializer
-  permission_classes = []
+  permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
   filter_backends = (filters.DjangoFilterBackend,)
-  filterset_fields = ['name']
-  
-  def create(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().create(request, *args, **kwargs)
-    return Response({'detail': 'Updates are not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-  
-  def list(self, request, *args, **kwargs):
-    return super().list(request, *args, **kwargs)
+  filterset_fields = CalendarSerializer.Meta.fields
 
-  def update(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().update(request, *args, **kwargs)
-    return Response({'detail': 'Updates are not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
+class CourseViewSet(viewsets.ModelViewSet):
+  """
+  API endpoint that allows courses to be viewed.
+  """
+  queryset = CourseModel.objects.all()
+  serializer_class = CourseSerializer
+  permission_classes = [permissions.DjangoObjectPermissions]
+  filter_backends = (filters.DjangoFilterBackend,)
+  filterset_fields = CourseSerializer.Meta.fields
 
-  def destroy(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().destroy(request, *args, **kwargs)
-    return Response({'detail': 'Deletion is not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-  
+class CurricularUnitViewSet(viewsets.ModelViewSet):
+  """
+  API endpoint that allows curricular units to be viewed.
+  """
+  queryset = CurricularUnitModel.objects.all()
+  serializer_class = CurricularUnitSerializer
+  permission_classes = [permissions.DjangoObjectPermissions]
+  filter_backends = (filters.DjangoFilterBackend,)
+  filterset_fields = CurricularUnitSerializer.Meta.fields
+
+class MaterialTagViewSet(viewsets.ModelViewSet):
+  """
+  API endpoint that allows material tags to be viewed.
+  """
+  queryset = MaterialTagModel.objects.all()
+  serializer_class = MaterialTagSerializer
+  permission_classes = [permissions.DjangoObjectPermissions]
+  filter_backends = (filters.DjangoFilterBackend,)
+  filterset_fields = MaterialTagSerializer.Meta.fields
+
+class MaterialViewSet(viewsets.ModelViewSet):
+  """
+  API endpoint that allows materials to be viewed or edited.
+  """
+  queryset = MaterialModel.objects.all()
+  serializer_class = MaterialSerializer
+  permission_classes = [permissions.DjangoObjectPermissions]
+  filter_backends = (filters.DjangoFilterBackend,)
+  filterset_fields = MaterialSerializer.Meta.fields
+
+class MaterialLinkViewSet(viewsets.ModelViewSet):
+  """
+  API endpoint that allows material links to be viewed or edited.
+  """
+  queryset = MaterialLinkModel.objects.all()
+  serializer_class = MaterialLinkSerializer
+  permission_classes = [permissions.DjangoObjectPermissions]
+  filter_backends = (filters.DjangoFilterBackend,)
+  filterset_fields = MaterialLinkSerializer.Meta.fields
+
 class MentorshipRequestViewSet(viewsets.ModelViewSet):
   """
   API endpoint that allows mentorship requests to be viewed or edited.
   """
   queryset = MentorshipRequestModel.objects.all()
   serializer_class = MentorshipRequestSerializer
-  permission_classes = [permissions.IsAuthenticated]
+  permission_classes = [permissions.DjangoObjectPermissions]
   filter_backends = (filters.DjangoFilterBackend,)
   filterset_fields = ['mentee', 'curricular_unit']
-  
+
   def create(self, request, *args, **kwargs):
     if isinstance(self.request.data, dict):
       self.request.data._mutable = True
     self.request.data['mentee'] = request.user.id
     return super().create(request, *args, **kwargs)
-  
-  def list(self, request, *args, **kwargs):
-    return super().list(request, *args, **kwargs)
-
-  def update(self, request, *args, **kwargs):
-    if request.user.is_superuser:
-      return super().update(request, *args, **kwargs)
-    return Response({'detail': 'Updates are not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
 
   def destroy(self, request, *args, **kwargs):
     if request.user.is_superuser:
@@ -228,53 +111,53 @@ class MentorshipRequestViewSet(viewsets.ModelViewSet):
     elif request.user == self.get_object().mentee:
       return super().destroy(request, *args, **kwargs)
     return Response({'detail': 'Deletion is not allowed for this resource.'}, status=status.HTTP_403_FORBIDDEN)
-  
+
 class MentorshipViewSet(viewsets.ModelViewSet):
   """
   API endpoint that allows mentorships to be viewed or edited.
   """
   queryset = MentorshipModel.objects.all()
   serializer_class = MentorshipSerializer
-  permission_classes = [permissions.IsAuthenticated]
+  permission_classes = [permissions.DjangoObjectPermissions]
   filter_backends = (filters.DjangoFilterBackend,)
-  filterset_fields = ['mentor', 'mentee', 'curricular_unit']
-  
+  filterset_fields = MentorshipSerializer.Meta.fields
+
 class MentorshipReviewViewSet(viewsets.ModelViewSet):
   """
   API endpoint that allows mentorship reviews to be viewed or edited.
   """
   queryset = MentorshipReviewModel.objects.all()
   serializer_class = MentorshipReviewSerializer
-  permission_classes = [permissions.IsAuthenticated]
+  permission_classes = [permissions.DjangoObjectPermissions]
   filter_backends = (filters.DjangoFilterBackend,)
-  filterset_fields = ['mentor', 'mentee', 'curricular_unit', 'rating']
-  
+  filterset_fields = MentorshipReviewSerializer.Meta.fields
+
 class BlogTopicViewSet(viewsets.ModelViewSet):
   """
   API endpoint that allows blog topics to be viewed or edited.
   """
   queryset = BlogTopicModel.objects.all()
   serializer_class = BlogTopicSerializer
-  permission_classes = [permissions.IsAuthenticated]
+  permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
   filter_backends = (filters.DjangoFilterBackend,)
-  filterset_fields = ['name']
-  
+  filterset_fields = BlogTopicSerializer.Meta.fields
+
 class BlogImageViewSet(viewsets.ModelViewSet):
   """
   API endpoint that allows blog images to be viewed or edited.
   """
   queryset = BlogImageModel.objects.all()
   serializer_class = BlogImageSerializer
-  permission_classes = [permissions.IsAuthenticated]
+  permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
   filter_backends = (filters.DjangoFilterBackend,)
-  filterset_fields = ['name']
-  
+  filterset_fields = BlogImageSerializer.Meta.fields
+
 class BlogPostViewSet(viewsets.ModelViewSet):
   """
   API endpoint that allows blog posts to be viewed or edited.
   """
   queryset = BlogPostModel.objects.all()
   serializer_class = BlogPostSerializer
-  permission_classes = [permissions.IsAuthenticated]
+  permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
   filter_backends = (filters.DjangoFilterBackend,)
-  filterset_fields = ['title', 'content', 'author']
+  filterset_fields = BlogPostSerializer.Meta.fields
