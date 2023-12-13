9c0fed221237d93191779e76b99925f93330b9bd
João Oliveira <joaopsoliveira2003@gmail.com>
2023-12-13 17:11:24 +0000

    Added Registration simple ViewSet and fixed Model that was causing OpenAPI schema to give errors

--- django/website/models.py
+++ django/website/models.py
@@ -239,7 +239,6 @@ def create_user_profile(sender, instance, created, **kwargs):
   if created:
     ProfileModel.objects.create(user=instance)
 
-
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
   instance.profilemodel.save()
