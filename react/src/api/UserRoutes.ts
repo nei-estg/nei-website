192c2362a90c1c4c58e96b990f47c6e1e5c2351e
João Oliveira <joaopsoliveira2003@gmail.com>
2024-01-25 15:08:00 +0000

    Remove CSP header and ajust api port

--- react/src/api/UserRoutes.ts
+++ react/src/api/UserRoutes.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://localhost',
 });
 
 export const loginUser = async (username: string, password: string) => {
