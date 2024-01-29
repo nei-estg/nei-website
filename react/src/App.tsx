35c6bbb619aa4a21388541db6928fe1cb2f8e50c
Miguel LS <115990649+Lsmiguel@users.noreply.github.com>
2024-01-29 17:21:37 +0000

    Refactor theme and footer component

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -5,27 +5,38 @@ import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import Footer from "./components/footer/Footer";
 import NavBar from "./components/NavBar";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import "./App.css";
 
 function App() {
+ 
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Cabin, sans-serif',
+  },
+});
+
   return (
-  <>
-    <CssBaseline />
-    <BrowserRouter>
-      <Box
-        sx={{
-          minHeight: "100vh",
-          bgcolor: "background.default",
-          color: "text.primary",
-        }}
-      >
-        <NavBar/>
-        <Container maxWidth={false} disableGutters={true}>
-          <Router />
-        </Container>
-        <Footer />
-      </Box>
-    </BrowserRouter>
-  </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Box
+          sx={{
+            fontFamily: "'Cabin', sans-serif",
+            minHeight: "100vh",
+            bgcolor: "background.default",
+            color: "text.primary",
+          }}
+        >
+          <NavBar />
+          <Container maxWidth={false} disableGutters={true}>
+            <Router />
+          </Container>
+          <Footer />
+        </Box>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- react/src/components/footer/Footer.tsx
+++ react/src/components/footer/Footer.tsx
@@ -1,80 +1,77 @@
-import React from 'react'
+import React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import { Box } from "@mui/material";
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import  GitHubIcon  from '@mui/icons-material/GitHub';
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import logo from "../../assets/logo.png";
 
 export const Footer = () => {
   return (
     <div>
-        <Box component="footer" style={{ backgroundColor: '#002855', color: '#ffffff' }}
-      sx={{
-        p: 6,
-      }}
-    >
-      <Container maxWidth="lg">
-        <Grid container spacing={5}>
-          <Grid item xs={12} sm={3}>
-            <Typography variant="h6" gutterBottom>
-              NEI
-            </Typography>
-            <Typography variant="body2">
-             Núcleo de Estudantes de Informática da ESTG
-            </Typography>
+      <Box
+        component="footer"
+        style={{ backgroundColor: "#002855", color: "#ffffff" }}
+        sx={{
+          p: 6,
+        }}
+      >
+        <Container maxWidth="lg">
+          <Grid container spacing={5}>
+            <Grid item xs={12} sm={5}>
+              <Grid container direction="row" alignItems="center">
+                <img
+                  src={logo}
+                  alt="logo"
+                  style={{ marginRight: "10px", height: "100px", width: "100px" }}
+                />
+                <div>
+                  <Typography variant="body2">NEI</Typography>
+                  <Typography variant="body2">Núcleo de Engenharia Informática da ESTG</Typography>
+                </div>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <Typography variant="h6" gutterBottom>
+                Contacto
+              </Typography>
+              <Typography variant="body2">
+                Email: nei@estg.ipp.pt (to be)
+              </Typography>
+              <Typography variant="body2">Tel: nao temos telemóvel</Typography>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Typography variant="h6" gutterBottom>
+                Redes Sociais
+              </Typography>
+              <Typography variant="body2">
+                <GitHubIcon color="primary" />
+              </Typography>
+              <Typography variant="body2">
+                <InstagramIcon color="primary" />
+              </Typography>
+              <Typography variant="body2">
+                <TwitterIcon color="primary" />
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={3}>
-            <Typography variant="h6" gutterBottom>
-              Morada
+          <Box mt={5}>
+            <Typography variant="body2" align="center">
+              {"Copyright © "}
+              <Link color="inherit" href="/">
+                NEI
+              </Link>{" "}
+              2023 - {new Date().getFullYear()}
+              {"."}
             </Typography>
-            <Typography variant="body2">
-              Rua do Curral, Casa do Curral, Margaride
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={3}>
-            <Typography variant="h6" gutterBottom>
-              Contacto
-            </Typography>
-            <Typography variant="body2">
-              Email: nei@estg.ipp.pt (to be)
-            </Typography>
-            <Typography variant="body2">
-              Tel: nao temos telemóvel
-            </Typography>  
-          </Grid>
-          <Grid item xs={12} sm={3}>
-            <Typography variant="h6" gutterBottom>
-              Redes Sociais
-            </Typography>
-            <Typography variant="body2">
-                <GitHubIcon color="primary"/>
-            </Typography>
-            <Typography variant="body2">
-                <InstagramIcon color="primary"/>
-            </Typography>
-            <Typography variant="body2">
-                <TwitterIcon color="primary"/>
-            </Typography>
-          </Grid>
-        </Grid>
-        <Box mt={5}>
-          <Typography variant="body2" align="center">
-            {"Copyright © "}
-            <Link color="inherit" href="/">
-              NEI
-            </Link>{" "}
-            2023 - {new Date().getFullYear()}
-            {"."}
-          </Typography>
-        </Box>
-      </Container>
-    </Box>
-  </div>
-  )
-}
+          </Box>
+        </Container>
+      </Box>
+    </div>
+  );
+};
 
-
-export default Footer
+export default Footer;
