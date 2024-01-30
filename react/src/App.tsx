747672ddfe02325e4d3767a80731d1449ee11851
Miguel LS <115990649+Lsmiguel@users.noreply.github.com>
2024-01-30 22:35:37 +0000

    Update import statement for Footer component

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -3,7 +3,7 @@ import Router from "./router/Router";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
-import Footer from "./components/footer/Footer";
+import {Footer} from "./components/footer/Footer";
 import NavBar from "./components/navbar/NavBar";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
