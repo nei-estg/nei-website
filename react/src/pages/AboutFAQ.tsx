257141e90e0f4aba86d06e3f3627b6fe4eef230c
heyliceeee <alicedias2002@hotmail.com>
2024-01-31 10:08:25 +0000

    :lipstick: feat: About Us (Direção)

--- react/src/pages/AboutFAQ.tsx
+++ react/src/pages/AboutFAQ.tsx
@@ -23,9 +23,9 @@ const direcao: DirecaoItem[] =
   {
     year: "2023/2024",
     team: [
-      { photo: "logo.png", role: "Presidente", fullname: "Hélder Branco" },
-      { photo: "logo.png", role: "Vice-Presidente", fullname: "Daniel Teixeira" },
-      { photo: "logo.png", role: "Vice-Presidente", fullname: "Emanuel Rego" },
+      { photo: "https://images.unsplash.com/photo-1581382575275-97901c2635b7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", role: "Presidente", fullname: "Hélder Branco" },
+      { photo: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bWFufGVufDB8MXwwfHx8MA%3D%3D", role: "Vice-Presidente", fullname: "Daniel Teixeira" },
+      { photo: "https://images.unsplash.com/photo-1590086782957-93c06ef21604?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", role: "Vice-Presidente", fullname: "Emanuel Rego" },
       { photo: "logo.png", role: "Tesoureiro", fullname: "João Oliveira" },
       { photo: "logo.png", role: "Secretário", fullname: "Hélder Carneiro" },
       { photo: "logo.png", role: "Vogal", fullname: "Mariana Martins" },
@@ -95,13 +95,15 @@ const faq: FaqSection[] =
   },
 ];
 
-const [value, setValue] = useState(0);
-
-const handleChange = (event: SyntheticEvent, newValue: number) => {
-  setValue(newValue);
-};
-
 export default function AboutFAQPage() {
+
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container maxWidth="xl" sx={{marginBottom: '60px'}}>
@@ -119,7 +121,8 @@ export default function AboutFAQPage() {
           }} 
         >Sobre Nós</Typography>
 
-
+      
+      {/*Direcao*/}
       <Typography variant="subtitle1"
         sx={{ 
           color: '#969696', 
@@ -131,33 +134,38 @@ export default function AboutFAQPage() {
           }} 
         >Direção</Typography>
 
-      
-        
-      {direcao.map((member) => (
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          variant="scrollable"
-          scrollButtons="auto"
-          aria-label="scrollable auto tabs example">
-
-          <Tab label={member.year} />
-        </Tabs>
-      ))}
-
-     {/*  <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        {direcao.map((member) => (
-          <Grid item xs={6} sm={3} md={2} key={member.fullname} display="flex" alignItems="center">
-            <Grid container direction="column" display="flex" alignItems="center" spacing={1} sx={{marginBottom: '5px'}}>
-              <Item>
-                <Avatar src={member.photo} sx={{ width: '100%', height: 'auto', marginBottom: '10px' }} variant="rounded"/>
-                <Typography variant="subtitle1" color="primary">{member.role}</Typography>
-                <Typography variant="body1">{member.fullname}</Typography>
-              </Item>
-            </Grid>
-          </Grid>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="scrollable auto tabs example"
+        sx={{marginBottom: '20px'}}>
+          
+        {direcao.map((member, index) => (
+          <Tab label={member.year} key={index} />
         ))}
-      </Grid> */}
+      </Tabs>
+
+      
+      {/* Conteudo de cada tab (cada ano) */}
+      {direcao.map((member, index) => (
+        <div key={index} style={{ display: value === index ? 'block' : 'none' }}>
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          {member.team.map((teamMember, teamIndex) => (
+            <Grid item xs={3} sm={2} md={2} key={teamIndex} display="flex" alignItems="center">
+              <Grid container direction="column" display="flex" alignItems="center" spacing={1} sx={{marginBottom: '5px'}}>
+                <Item>
+                  <Avatar src={teamMember.photo} sx={{ width: '128px', height: '128px', marginBottom: '10px' }} variant="rounded"/>
+                  <Typography variant="subtitle1" color="primary">{teamMember.role}</Typography>
+                  <Typography variant="body1">{teamMember.fullname}</Typography>
+                </Item>
+              </Grid>
+            </Grid>
+          ))}
+          </Grid>
+        </div>
+      ))}
 
 
 
@@ -190,24 +198,23 @@ export default function AboutFAQPage() {
 
 
       {/* QA */}
-      {faq.map((faq) => (
-        <>
-        <Typography variant="h5"
-          sx={{
-            color: '#1E2022',
-            display: 'flex',
-            fontWeight: 700,
-            flexDirection: 'column',
-            alignItems: 'left',
-            marginTop: '55px',
-            marginBottom: '10px',
-          }}
-        >{faq.section}</Typography>
-        
-          {faq.qa.map((qa) => (
-            <AccordionUsage question={qa.question} answer={qa.answer}/>
-          ))}
-        </>
+      {faq.map((faq, index) => (
+        <div key={index}>
+          <Typography variant="h5"
+            sx={{
+              color: '#1E2022',
+              display: 'flex',
+              fontWeight: 700,
+              flexDirection: 'column',
+              alignItems: 'left',
+              marginTop: '55px',
+              marginBottom: '10px',
+            }}>{faq.section}</Typography>
+          
+            {faq.qa.map((qa, qaIndex) => (
+              <AccordionUsage key={qaIndex} question={qa.question} answer={qa.answer}/>
+            ))}
+        </div>
       ))}
       
       </Container>
