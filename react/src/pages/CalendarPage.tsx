eb71d926c24a0a1ce5f4efcc30963f5208e11c13
Miguel LS <115990649+Lsmiguel@users.noreply.github.com>
2024-02-03 22:38:46 +0000

    Calendar Style v1

--- react/src/pages/CalendarPage.tsx
+++ react/src/pages/CalendarPage.tsx
@@ -1,10 +1,31 @@
-import { MyCalendar } from "@src/components/calendar/calendar"
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import events from "../components/calendar/events"
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from "react";
+
+moment.locale("en-GB");
+const localizer = momentLocalizer(moment);
 
 export default function CalendarPage() {
+  const [eventsData, setEventsData] = useState(events);
+
+  const handleSelect = () => {
+    const title = window.prompt("New Event name");
+  };
   return (
-    <>
-      <p>CalendarPage</p>
-      <MyCalendar />
-    </>
-  )
+    <div className="App">
+      <Calendar
+        views={["day", "agenda", "work_week", "month"]}
+        selectable
+        localizer={localizer}
+        defaultDate={new Date()}
+        defaultView="month"
+        events={eventsData}
+        style={{ height: "100vh" }}
+        onSelectEvent={(event) => alert(event.title)}
+        onSelectSlot={handleSelect}
+      />
+    </div>
+  );
 }
