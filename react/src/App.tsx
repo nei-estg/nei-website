2ee2401a8c5540be64437b27510638d99229227d
João Oliveira <joaopsoliveira2003@gmail.com>
2024-02-07 11:47:33 +0000

    try add developer console warning and remove particles console log

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -4,12 +4,12 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import FooterWrapper from "./components/footer/FooterWrapper";
-import NavBar from "./components/navbar/NavBar";
 import NavBarWrapper from "./components/navbar/NavBarWrapper";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@mui/material/styles';
+import { useEffect } from "react";
 
 function App() {
   const theme = createTheme({
@@ -17,6 +17,20 @@ function App() {
       fontFamily: 'Cabin, sans-serif',
     },
   });
+
+  useEffect(() => {
+    const handler = () => {
+      console.log('Developer console is open!');
+      // You can replace the above log with any action you want to take when the console is opened
+      alert('Developer console is open!');
+    };
+
+    window.addEventListener('devtoolschange', handler);
+
+    return () => {
+      window.removeEventListener('devtoolschange', handler);
+    };
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
--- react/src/components/particles/particles.tsx
+++ react/src/components/particles/particles.tsx
@@ -4,90 +4,93 @@ import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim";
 
 export const Particle = () => {
-    const particlesInit = useCallback(async (engine: Engine) => {
-        console.log(engine);
-        await loadSlim(engine);
-    }, []);
+  const particlesInit = useCallback(async (engine: Engine) => {
+    //console.log(engine);
+    await loadSlim(engine);
+  }, []);
 
-    const particlesLoaded = useCallback(async (container: Container | undefined) => {
-        await console.log(container);
-    }, []);
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      //await console.log(container);
+      await container;
+    },
+    []
+  );
 
-    return (
-      <Particles
-          id="tsparticles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={{
-              background: {
-                  color: {
-                      value: "#191919",
-                  },
-              },
-              fpsLimit: 100,
-              interactivity: {
-                  events: {
-                      onClick: {
-                          enable: true,
-                          mode: "push",
-                      },
-                      onHover: {
-                          enable: true,
-                          mode: "repulse",
-                      },
-                      resize: true,
-                  },
-                  modes: {
-                      push: {
-                          quantity: 8,
-                      },
-                      repulse: {
-                          distance: 120,
-                          duration: 0.4,
-                      },
-                  },
-              },
-              particles: {
-                  color: {
-                      value: "#ffffff",
-                  },
-                  links: {
-                      color: "#ffffff",
-                      distance: 100,
-                      enable: true,
-                      opacity: 0.5,
-                      width: 1,
-                  },
-                  move: {
-                      direction: "none", // "none", "top", "top-right", "right", "bottom-right", "bottom", "bottom-left", "left", "top-left"
-                      enable: true,
-                      outModes: {
-                          default: "bounce", // "bounce" | "none" | OutMode | "bounceHorizontal" | "bounceVertical" | "out" | "destroy" | "split"
-                      },
-                      random: false,
-                      speed: 1,
-                      straight: false,
-                  },
-                  number: {
-                      density: {
-                          enable: true,
-                          area: 800, // Here, we can change the area of particles. Lower number = more particles
-                      },
-                      value: 50, // Here, we can change the number of particles
-                  },
-                  opacity: {
-                      value: 0.5,
-                  },
-                  shape: {
-                      type: "circle", // "circle", "square", "triangle", "polygon", "star", "image"
-                  },
-                  size: {
-                      value: { min: 1, max: 5 },
-                  },
-              },
-              detectRetina: true,
-          }}
-      />
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#191919",
+          },
+        },
+        fpsLimit: 100,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 8,
+            },
+            repulse: {
+              distance: 120,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 100,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          move: {
+            direction: "none", // "none", "top", "top-right", "right", "bottom-right", "bottom", "bottom-left", "left", "top-left"
+            enable: true,
+            outModes: {
+              default: "bounce", // "bounce" | "none" | OutMode | "bounceHorizontal" | "bounceVertical" | "out" | "destroy" | "split"
+            },
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800, // Here, we can change the area of particles. Lower number = more particles
+            },
+            value: 50, // Here, we can change the number of particles
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle", // "circle", "square", "triangle", "polygon", "star", "image"
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
   );
 };
-
