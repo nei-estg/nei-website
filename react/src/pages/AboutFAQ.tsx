02825c604569f511f9c3791e1efdaf5268395ee6
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2024-02-09 14:37:31 +0000

    :lipstick: courses

--- react/src/pages/AboutFAQ.tsx
+++ react/src/pages/AboutFAQ.tsx
@@ -58,26 +58,28 @@ interface CoursesItem {
 const courses: CoursesItem[] =
   [
     {
-      type: "CTESP",
+      type: "CTeSP",
       course: [
-        { name: "Curso Técnico Superior Profissional em Cibersegurança, Redes e Sistemas Informáticos", sigla: "CRSI", photo: "", URL: "https://www.estg.ipp.pt/cursos/ctesp/810" },
-        { name: "Curso Técnico Superior Profissional em Desenvolvimento para a Web e Dispositivos Móveis", sigla: "DWDM", photo: "", URL: "https://www.estg.ipp.pt/cursos/ctesp/809" },
+        { name: "CTeSP Desenvolvimento para a Web e Dispositivos Móveis", sigla: "DWDM", photo: "https://media.discordapp.net/attachments/697858645559476265/1205208469951619123/image.png?ex=65d788da&is=65c513da&hm=6c703fbf0325feea6c8c699387b378ac70ec80465eb64a23d4dbba3d6d415e47&=&format=webp&quality=lossless&width=546&height=701", URL: "https://www.estg.ipp.pt/cursos/ctesp/809" },
+        { name: "CTeSP Cibersegurança, Redes e Sistemas Informáticos", sigla: "CRSI", photo: "https://media.discordapp.net/attachments/697858645559476265/1205208641062441000/image.png?ex=65d78903&is=65c51403&hm=89257a0ad5527ba7cef7eb1dc692602a85723bddf79be503be850f2c70010f17&=&format=webp&quality=lossless&width=547&height=700", URL: "https://www.estg.ipp.pt/cursos/ctesp/810" },
+        { name: "CTeSP Tecnologias de Dados e Visualização de Informação", sigla: "TDVI", photo: "https://media.discordapp.net/attachments/697858645559476265/1205209113953575063/image.png?ex=65d78974&is=65c51474&hm=d88cfd622cb16d10af2ce4c7a1d0cfca8286b5f1fe852f41f1496781d5d556eb&=&format=webp&quality=lossless&width=528&height=701", URL: "https://www.estg.ipp.pt/cursos/ctesp/840" },
+
       ]
     },
 
     {
       type: "Licenciatura",
       course: [
-        { name: "Licenciatura em Segurança Informática em Redes de Computadores", sigla: "LSRIC", photo: "", URL: "https://www.estg.ipp.pt/cursos/licenciatura/557" },
-        { name: "Licenciatura em Engenharia Informática", sigla: "LEI", photo: "", URL: "https://www.estg.ipp.pt/cursos/licenciatura/551" },
-        { name: "Licenciatura em Sistemas de Informação para a Gestão", sigla: "LSIG", photo: "", URL: "https://www.estg.ipp.pt/cursos/licenciatura/630" },
+        { name: "Segurança Informática em Redes de Computadores", sigla: "LSRIC", photo: "https://media.discordapp.net/attachments/697858645559476265/1205222934696689684/image.png?ex=65d79653&is=65c52153&hm=c7c719916dda26fb1690107d20a1ed4ca929f80f245568782aa365f36b07cc53&=&format=webp&quality=lossless&width=539&height=701", URL: "https://www.estg.ipp.pt/cursos/licenciatura/557" },
+        { name: "Engenharia Informática", sigla: "LEI", photo: "https://media.discordapp.net/attachments/697858645559476265/1205222755536994324/image.png?ex=65d79628&is=65c52128&hm=97afd2f66f47f956f9f140134560a343d4f0f904721dc638aa384a95a4e76305&=&format=webp&quality=lossless&width=532&height=700", URL: "https://www.estg.ipp.pt/cursos/licenciatura/551" },
+        { name: "Sistemas de Informação para a Gestão", sigla: "LSIG", photo: "https://media.discordapp.net/attachments/697858645559476265/1205223056834826280/image.png?ex=65d79670&is=65c52170&hm=0d7561c695e4bcb3f1662d93aa59aebdac88d8888a47622ed6e74d2d0ed991e5&=&format=webp&quality=lossless&width=541&height=701", URL: "https://www.estg.ipp.pt/cursos/licenciatura/630" },
       ]
     },
 
     {
       type: "Mestrado",
       course: [
-        { name: "Mestrado em Engenharia Informática", sigla: "MEI", photo: "", URL: "https://www.estg.ipp.pt/cursos/mestrado/807" },
+        { name: "Mestrado Engenharia Informática", sigla: "MEI", photo: "https://media.discordapp.net/attachments/697858645559476265/1205207215540142130/image.png?ex=65d787af&is=65c512af&hm=00cd1de700dec73de23808c45b3ca50c2102757a4fc3f0f295ba642498ee8025&=&format=webp&quality=lossless&width=562&height=701", URL: "https://www.estg.ipp.pt/cursos/mestrado/807" },
       ]
     },
   ];
@@ -352,17 +354,19 @@ export default function AboutFAQPage() {
         {courses.map((courses, index) => (
           <div key={index} style={{ display: valueCourse === index ? 'block' : 'none' }}>
             <Grid container spacing={1} justifyContent="center">
+
               {courses.course.map((course, courseIndex) => (
                 <Grid item xs={12} sm={4} md={3} lg={2} xl={1} key={courseIndex} display="flex" sx={{ marginBottom: '1px', [defaultTheme.breakpoints.up('xl')]: { marginRight: '41px', }, }}>
                   <Grid container direction="column" display="flex">
-                    <Item>
-                      <Avatar src={course.photo} sx={{ width: '115px', height: '128px', marginBottom: '10px' }} variant="rounded" />
+                    <Item onClick={() => window.open(course.URL, '_blank')} style={{ cursor: 'pointer', textAlign: 'center' }}>
+                      <Avatar src={course.photo} sx={{ width: '115px', height: 'auto', objectFit: 'cover', marginBottom: '10px', display: 'block', margin: '0 auto' }} variant="rounded" />
                       <Typography variant="subtitle1" color="primary">{course.sigla}</Typography>
                       <Typography variant="subtitle2">{course.name}</Typography>
                     </Item>
                   </Grid>
                 </Grid>
               ))}
+
             </Grid>
           </div>
         ))}
