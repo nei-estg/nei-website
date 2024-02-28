eaf23dcc0810ffb3d8fe6576d871497c9a94429c
heyliceeee <alicedias2002@hotmail.com>
2024-02-28 19:09:20 +0000

    :lipstick: mentoring request list & mentoring list

--- react/src/pages/MentoringPage.tsx
+++ react/src/pages/MentoringPage.tsx
@@ -1,12 +1,16 @@
 import { createMentoring, createMentoringRequest, getMentoringList, getMentoringRequestList } from "@src/api/MentoringRoutes"
 import { useEffect, useState } from "react";
 import { toast, Bounce } from "react-toastify";
+import CheckIcon from '@mui/icons-material/Check';
 import { IMentoringRequest } from "@src/interfaces/IMentoringRequest";
 import { IMentoring } from "@src/interfaces/IMentoring";
 import { Box, Button, Container, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, ThemeProvider, Typography, createTheme } from "@mui/material";
 import { ICurricularUnit } from "@src/interfaces/ICurricularUnit";
 import { getCurricularUnitsCourseUser } from "@src/api/CourseRoutes";
 import InfoIcon from '@mui/icons-material/Info';
+import CancelIcon from '@mui/icons-material/Cancel';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+
 
 const defaultTheme = createTheme();
 
@@ -157,14 +161,14 @@ export default function MentoringPage() {
     );
   };
 
+
+
+
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container maxWidth="xl" sx={{ marginBottom: "60px" }}>
-        <Typography variant="h3" align="left" gutterBottom sx={{ marginTop: "30px", marginBottom: "30px" }}>
-          Mentoria
-        </Typography>
-
-        <Typography variant="body1" align="center" gutterBottom sx={{ marginTop: "0px", marginBottom: "30px" }}>
+        <Typography variant="body1" align="center" gutterBottom sx={{ marginTop: "30px", marginBottom: "30px" }}>
           <InfoIcon sx={{marginRight: '5px', color: "#054496", marginBottom: "-3px"}}/>
           A tua privacidade é uma prioridade. 
           Por isso, não vais saber quem é a outra pessoa até aceitares a mentoria. 
@@ -172,44 +176,23 @@ export default function MentoringPage() {
           É simples e seguro, garantindo que escolhes com quem queres partilhar informações.
         </Typography>
 
-        <Typography variant="h5" align="left" gutterBottom sx={{ marginTop: "30px", marginBottom: "30px" }}>
-          Lista de Pedidos de Mentoria
-        </Typography>
-        {mentoringRequestList.map((mentoringRequest) => (
-          <Box
-            component="form"
-            onSubmit={handleCreateMentoring}
+        <Typography variant="h4" 
+          sx={{ 
+            color: "#1E2022",
+            display: "flex",
+            fontWeight: 700,
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: "30px",
+            marginBottom: "0px",
+          }}
           >
-            <TextField
-              name="requestId"
-              value={mentoringRequest.id}
-              sx={{ display: "none" }}
-            />
-            <p>Pedido - {mentoringRequest.curricularUnit.abbreviation}</p>
-            <Button
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              type="submit"
-            >
-              Aceitar Mentoria
-            </Button>
-          </Box>
-        ))}
-
-        <Typography variant="h5" align="left" gutterBottom sx={{ marginTop: "30px", marginBottom: "30px" }}>
-          Lista de Mentorias
-        </Typography>
-        {mentoringList.map((mentoring) => (
-          <p key={mentoring.id}>{mentoring.curricularUnit?.abbreviation} - {mentoring.mentor?.username} - {mentoring.mentee?.username}</p>
-        ))}
-       
-        <Typography variant="h5" align="left" gutterBottom sx={{ marginTop: "30px", marginBottom: "0px" }}>
           Criar Pedido de Mentoria
         </Typography>
+
         <Box
           component="form"
-          onSubmit={handleCreateMentoringRequest}
+          onSubmit={handleCreateMentoringRequest} sx={{width: '70%', justifyContent: 'center', alignContent: 'center'}}
         >
           <FormControl fullWidth sx={{ mt: 2 }}>
             <InputLabel id="curricular-unit-label">
@@ -238,8 +221,91 @@ export default function MentoringPage() {
             sx={{ mt: 3, mb: 2 }}
             type="submit"
           >
-            Criar Pedido de Mentoria
+            Criar
           </Button>
+        </Box>
+
+        <Typography variant="h4" 
+          sx={{ 
+            color: "#1E2022",
+            display: "flex",
+            fontWeight: 700,
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: "30px",
+            marginBottom: "15px",
+          }}
+          >
+          Lista de Pedidos de Mentoria
+        </Typography>
+
+        <Box sx={{ display: 'flex', justifyContent: 'center', gap: '20px', flexWrap: 'wrap' }}>
+          {mentoringRequestList.map((mentoringRequest) => (
+            <Box
+              key={mentoringRequest.id} // Adiciona a chave de identificação
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignContent: 'center',
+                flexDirection: 'column', // Para colocar os elementos um em cima do outro
+                boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', // Adiciona uma sombra
+                padding: '20px', // Adiciona preenchimento para destacar a sombra
+                borderRadius: '8px', // Adiciona bordas arredondadas para um visual mais suave
+                width: '20%',
+              }}
+              component="form"
+              onSubmit={handleCreateMentoring} // Passa o evento e o ID do pedido de mentoria
+            >
+              <TextField name="requestId" value={mentoringRequest.id} sx={{ display: "none" }} />
+              <Typography variant="subtitle1" align="center" gutterBottom>
+                {mentoringRequest.curricularUnit.name}
+              </Typography>
+              <Button variant="contained" sx={{ mt: 2 }} type="submit" color="success"><CheckIcon/></Button>
+            </Box>
+          ))}
+        </Box>
+
+
+
+        <Typography variant="h4" 
+          sx={{ 
+            color: "#1E2022",
+            display: "flex",
+            fontWeight: 700,
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: "60px",
+            marginBottom: "15px",
+          }}
+          >
+          Lista de Mentorias
+        </Typography>
+
+        <Box sx={{ display: 'flex', justifyContent: 'center', gap: '20px', flexWrap: 'wrap' }}>
+          {mentoringList.map((mentoring) => (
+            <Box
+             key={mentoring.id} // Adiciona a chave de identificação
+             sx={{
+               display: 'flex',
+               justifyContent: 'center',
+               alignContent: 'center',
+               flexDirection: 'column',
+               width: '20%',
+               boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', // Adiciona uma sombra
+               padding: '20px', // Adiciona preenchimento para destacar a sombra
+               borderRadius: '8px', // Adiciona bordas arredondadas para um visual mais suave
+             }}
+           >
+
+          <TextField name="requestId" value={mentoring.id} sx={{ display: "none" }} />
+            <Typography variant="subtitle1" align="center" gutterBottom>
+              {mentoring.curricularUnit?.name}: {mentoring.mentee?.profilemodel.discord}
+            </Typography>
+            
+              <Button variant="contained" sx={{ mt: 2 }}><DoneAllIcon/></Button>
+              <Button variant="outlined" sx={{ mt: 1 }} color="error"><CancelIcon/></Button>
+            </Box>
+          ))}
         </Box>
       </Container>
     </ThemeProvider>
