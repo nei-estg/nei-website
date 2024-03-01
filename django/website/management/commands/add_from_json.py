5c5218a5297027d22c0100b1c39ff44796e092f6
João Oliveira <joaopsoliveira2003@gmail.com>
2024-03-01 15:31:07 +0000

    Fix Add Data from Json

--- django/website/management/commands/add_from_json.py
+++ django/website/management/commands/add_from_json.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from website.models import CourseModel, CurricularUnitModel, FAQCategoryModel, FAQModel, MaterialTagModel, BlogTopicModel
 import json
+from django.db import transaction
 
 
 class Command(BaseCommand):
@@ -16,91 +17,131 @@ class Command(BaseCommand):
     self.update_faqs(data.get('FAQs', []))
 
   def update_courses(self, courses_data):
+    #! Create Courses
     for course_data in courses_data:
-      course, _ = CourseModel.objects.get_or_create(
-        name=course_data['name'],
-        abbreviation=course_data['abbreviation']
+      course, created = CourseModel.objects.get_or_create(
+        abbreviation=course_data['abbreviation'],
+        name=course_data['name']
+      )
+      
+      if created:
+        self.stdout.write(self.style.SUCCESS(
+          f'Created new course: {course}'))
+      else:
+        self.stdout.write(self.style.WARNING(
+          f'Course: {course}'))
+    
+    #! For Each Course
+    for course_data in courses_data:
+      course = CourseModel.objects.get(
+        abbreviation=course_data['abbreviation'],
+        name=course_data['name']
       )
       curricular_units_data = course_data.get('curricularUnits', {})
+      
+      #! For Each Year
       for year, curricular_units in curricular_units_data.items():
+          
+        #! For Each Curricular Unit
         for unit_data in curricular_units:
-          if 'together' in unit_data:
-            self.stdout.write(self.style.SUCCESS(
-              f'Processing together curricular units for {unit_data["abbreviation"]}'))
-            for course_abbreviation in unit_data['together']:
-              course_together = CourseModel.objects.filter(abbreviation=course_abbreviation)
-              if course_together.exists():
-                self.stdout.write(self.style.SUCCESS(
-                  f'Found Course {course_together[0]}'))
-                for course_t in course_together:
-                  curricular_unit = CurricularUnitModel.objects.filter(
-                    abbreviation=unit_data['abbreviation'],
-                    course=course_t
-                  )
-                  if curricular_unit.exists():
-                    curricular_unit[0].course.add(course_t)
-                    self.stdout.write(self.style.SUCCESS(
-                      f'Added Course {course_t} to existing Curricular Unit {curricular_unit[0]}'))
-                  else:
-                    self.stdout.write(self.style.SUCCESS(
-                      f'Course {course_t} not found. Adding curricular unit to current course'))
-                    unit = CurricularUnitModel.objects.create(
-                      name=unit_data['name'],
-                      abbreviation=unit_data['abbreviation'],
-                      year=year
-                    )
-                    unit.course.add(course_t)
-                    self.stdout.write(self.style.SUCCESS(
-                      f'Created new Curricular Unit {unit}'))
-              else:
-                self.stdout.write(self.style.SUCCESS(
-                  f'Course {course_abbreviation} not found. Adding curricular unit to current course'))
-                unit = CurricularUnitModel.objects.create(
-                  name=unit_data['name'],
-                  abbreviation=unit_data['abbreviation'],
-                  year=year
-                )
-                unit.course.add(course)
-                self.stdout.write(self.style.SUCCESS(
-                  f'Created new Curricular Unit {unit}'))
-          else:
-            existing_unit = CurricularUnitModel.objects.filter(
-              abbreviation=unit_data['abbreviation'],
-              course=course
+            
+          #! Check if the curricular unit already exists
+          curricular_unit = CurricularUnitModel.objects.filter(
+            abbreviation = unit_data['abbreviation'],
+            name=unit_data['name'],
+            course=course,
+            year=year
+          )
+          
+          #! If it doesn't exist, create it
+          if not curricular_unit:
+            curricular_unit = CurricularUnitModel(
+              abbreviation = unit_data['abbreviation'],
+              name=unit_data['name'],
+              year=year
             )
-            if not existing_unit.exists():
-              unit = CurricularUnitModel.objects.create(
-                name=unit_data['name'],
-                abbreviation=unit_data['abbreviation'],
-                year=year
-              )
-              unit.course.add(course)
-              self.stdout.write(self.style.SUCCESS(
-                f'Created new Curricular Unit {unit}'))
+            curricular_unit.save()
+            curricular_unit.course.add(course)
+            curricular_unit.save()
+            self.stdout.write(self.style.SUCCESS(
+              f'Curricular unit: {curricular_unit}'))
+            
+            #! Add together courses
+            if 'together' in unit_data:
+              for together_course_json in unit_data['together']:
+                together_course = CourseModel.objects.get(
+                  abbreviation=together_course_json,
+                )
+                curricular_unit.course.add(together_course)
+                curricular_unit.save()
+                self.stdout.write(self.style.SUCCESS(
+                  f'Together Course: {together_course} to {curricular_unit}'))
+          else:
+              #! If it exists, get the first one since there should be only one
+              curricular_unit = curricular_unit[0]
+              if 'together' in unit_data:
+                for together_course_json in unit_data['together']:
+                  together_course = CourseModel.objects.get(
+                    abbreviation=together_course_json,
+                  )
+                  
+                  #! Check if the together course is already added to the curricular unit
+                  if not curricular_unit.course.filter(abbreviation=together_course_json).exists():
+                    curricular_unit.course.add(together_course)
+                    curricular_unit.save()
+                    self.stdout.write(self.style.SUCCESS(
+                      f'Together Course: {together_course} to {curricular_unit}'))
+                  else:
+                    self.stdout.write(self.style.WARNING(
+                      f'Together Course: {together_course} already added to {curricular_unit}'))
 
   def update_material_tags(self, material_tags_data):
-      for tag_name in material_tags_data:
-          tag, created = MaterialTagModel.objects.get_or_create(
-              name=tag_name)
+    for tag_name in material_tags_data:
+      tag, created = MaterialTagModel.objects.get_or_create(
+        name=tag_name)
+      
+      if created:
+        self.stdout.write(self.style.SUCCESS(
+          f'Created new material tag: {tag}'))
+      else:
+        self.stdout.write(self.style.WARNING(
+          f'Material Tag: {tag}'))
 
   def update_blog_topics(self, blog_topics_data):
-      for topic_name in blog_topics_data:
-          topic, created = BlogTopicModel.objects.get_or_create(
-              name=topic_name)
+    for topic_name in blog_topics_data:
+      topic, created = BlogTopicModel.objects.get_or_create(
+        name=topic_name)
+      
+      if created:
+        self.stdout.write(self.style.SUCCESS(
+          f'Created new blog topic: {topic}'))
+      else:
+        self.stdout.write(self.style.WARNING(
+          f'Blog Topic: {topic}'))
 
   def update_faqs(self, faqs_data):
-      for category_faqs in faqs_data:
-          for category_name, faqs_list in category_faqs.items():
-              category, created = FAQCategoryModel.objects.get_or_create(
-                  name=category_name)
-              self.stdout.write(self.style.SUCCESS(
-                  f'Created new FAQ category: {category}'))
+    for category_faqs in faqs_data:
+      for category_name, faqs_list in category_faqs.items():
+        category, created = FAQCategoryModel.objects.get_or_create(
+          name=category_name)
 
-              for faq_data in faqs_list:
-                  faq, created = FAQModel.objects.get_or_create(
-                      question=faq_data['question'],
-                      answer=faq_data['answer'],
-                      category=category
-                  )
-                  self.stdout.write(self.style.SUCCESS(
-                      f'Created new FAQ: {faq}'))
+        if created:
+          self.stdout.write(self.style.SUCCESS(
+            f'Created new FAQ category: {category}'))
+        else:
+          self.stdout.write(self.style.WARNING(
+            f'FAQ Category: {category}'))
+        
+        for faq_data in faqs_list:
+          faq, created = FAQModel.objects.get_or_create(
+            question=faq_data['question'],
+            answer=faq_data['answer'],
+            category=category
+          )
+          
+          if created:
+            self.stdout.write(self.style.SUCCESS(
+              f'Created new FAQ: {faq}'))
+          else:
+            self.stdout.write(self.style.WARNING(
+              f'FAQ: {faq}'))
